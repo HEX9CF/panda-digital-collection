0f4a957a8f3dbaed904c63c5b4e1baeaf1f4078c
HEX9CF <hex9cf@aliyun.com>
2023-10-21 11:51:19 +0800

    安装element-plus

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -3,12 +3,36 @@ import App from './App.vue'
 
 // 导入插件
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-// 创建app实例
-const app = createApp(App)
+// 创建App实例
+const app = createApp(App);
 
 // 使用插件
-app.use(router)
+app.use(router);
+app.use(ElementPlus);
+
+// 导入所有图标并进行全局注册
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+// 修改页面title
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
+
+// 禁用右键
+// document.oncontextmenu = function () { return false; }
+// 禁用选择
+// document.onselectstart = function () { return false; }
+// 禁用F12
+// document.οnkeydοwn = function () { return false; }
 
 // 挂载app
-app.mount('#app')
+app.mount('#app');
